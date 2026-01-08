19b9e489e5a78db6cd52e9f7396c85c25334d565
bitmuzi <muzi_novice@163.com>
2026-01-08 16:00:31 +0800

    工程项目生成 (工具调用流式输出)
    1. 定制LangChain4j源码, 给TokenStream增加工具调用流式输出
    2. 定义统一的流式消息格式
    3. 修改AI门面服务, 通过适配器将Vue生成模式的TokenStream转为Flux(同时修改了AI方法的返回值为TokenStream)
    4. 定义了消息流处理器, 支持对不同模式消息进行统一处理
    5. 简化了AppServiceImpl和AI对话代码

--- src/main/java/org/example/ai/service/impl/AppServiceImpl.java
+++ src/main/java/org/example/ai/service/impl/AppServiceImpl.java
@@ -10,6 +10,7 @@ import com.mybatisflex.spring.service.impl.ServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.example.ai.constant.AppConstant;
 import org.example.ai.core.AiCodeGeneratorFacade;
+import org.example.ai.core.handler.StreamHandlerExecutor;
 import org.example.ai.exception.BusinessException;
 import org.example.ai.exception.ErrorCode;
 import org.example.ai.exception.ThrowUtils;
@@ -53,6 +54,8 @@ public class AppServiceImpl extends ServiceImpl<AppMapper, App> implements AppSe
     private AiCodeGeneratorFacade aiCodeGeneratorFacade;
     @Resource
     private ChatHistoryService chatHistoryService;
+    @Resource
+    private StreamHandlerExecutor streamHandlerExecutor;
 
     @Override
     public AppVO getAppVO(App app) {
@@ -139,27 +142,9 @@ public class AppServiceImpl extends ServiceImpl<AppMapper, App> implements AppSe
         // 5. 通过校验后，添加用户消息到对话历史
         chatHistoryService.addChatMessage(appId, message, ChatHistoryMessageTypeEnum.USER.getValue(), loginUser.getId());
         // 6. 调用 AI 生成代码（流式）
-        Flux<String> contentFlux = aiCodeGeneratorFacade.generateAndSaveCodeStream(message, codeGenTypeEnum, appId);
+        Flux<String> codeStream = aiCodeGeneratorFacade.generateAndSaveCodeStream(message, codeGenTypeEnum, appId);
         // 7. 收集AI响应内容并在完成后记录到对话历史
-        StringBuilder aiResponseBuilder = new StringBuilder();
-        return contentFlux
-                .map(chunk -> {
-                    // 收集AI响应内容
-                    aiResponseBuilder.append(chunk);
-                    return chunk;
-                })
-                .doOnComplete(() -> {
-                    // 流式响应完成后，添加AI消息到对话历史
-                    String aiResponse = aiResponseBuilder.toString();
-                    if (StrUtil.isNotBlank(aiResponse)) {
-                        chatHistoryService.addChatMessage(appId, aiResponse, ChatHistoryMessageTypeEnum.AI.getValue(), loginUser.getId());
-                    }
-                })
-                .doOnError(error -> {
-                    // 如果AI回复失败，也要记录错误消息
-                    String errorMessage = "AI回复失败: " + error.getMessage();
-                    chatHistoryService.addChatMessage(appId, errorMessage, ChatHistoryMessageTypeEnum.AI.getValue(), loginUser.getId());
-                });
+        return streamHandlerExecutor.doExecute(codeStream, chatHistoryService, appId, loginUser, codeGenTypeEnum);
     }
 
     @Override
